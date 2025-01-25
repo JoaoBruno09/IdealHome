12587f7497ecf6c48eea8e0a95012ab38ccb3e63
João Rocha <1220256@isep.ipp.pt>
2025-01-25 17:13:22 +0000

    Created a Spring Boot Application

---
+++ app/src/main/java/com/api/idealhome/IdealhomeApplication.java
@@ -0,0 +1,13 @@
+package com.api.idealhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealhomeApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/api/idealhome/IdealhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.idealhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
