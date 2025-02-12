068db9ef7c04b93288e284f948235a73a8d6a124
JoaoBruno1220256 <1220256@isep.ipp.pt>
2025-02-12 23:30:48 +0000

    Created notion new endpoint to add properties into a new database

--- idealhome/src/main/java/com/api/idealhome/clients/NotionClient.java
+++ idealhome/src/main/java/com/api/idealhome/clients/NotionClient.java
@@ -1,18 +1,25 @@
 package com.api.idealhome.clients;
 
+import com.api.idealhome.models.dtos.NotionRequestDTO;
 import com.api.idealhome.models.dtos.NotionResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(value = "notionAPI", url = "${notion.url}")
 public interface NotionClient {
 
-    @PostMapping(value = "/databases/{databaseId}/query", headers = "")
+    @PostMapping(value = "/databases/{databaseId}/query")
     NotionResponseDTO getPropertiesDataBase(@RequestHeader("Authorization") String token,
                                             @RequestHeader("grant_type") String grantType,
                                             @RequestHeader("scope") String scope,
                                             @RequestHeader("Notion-Version") String notionVersion,
                                             @PathVariable("databaseId") String databaseId);
+
+    @PostMapping(value = "/pages")
+    void addNewProperty(@RequestHeader("Authorization") String token,
+                        @RequestHeader("Notion-Version") String notionVersion,
+                        @RequestBody NotionRequestDTO notionRequestDTO);
 }
