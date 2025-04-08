c937d66a06484780f4dcb46853d2c64ac97ab908
JoaoBruno1220256 <1220256@isep.ipp.pt>
2025-04-09 00:06:45 +0100

    Deleted tests and created war file instead of jar

--- idealhome/src/main/java/com/api/idealhome/IdealhomeApplication.java
+++ idealhome/src/main/java/com/api/idealhome/IdealhomeApplication.java
@@ -2,13 +2,14 @@ package com.api.idealhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
-public class IdealhomeApplication {
+public class IdealhomeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(IdealhomeApplication.class, args);
