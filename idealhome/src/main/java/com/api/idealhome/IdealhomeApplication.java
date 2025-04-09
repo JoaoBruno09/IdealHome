0d59193e388b661a5dd1c0232be12ce982f95464
JoaoBruno1220256 <1220256@isep.ipp.pt>
2025-04-09 11:14:43 +0100

    Fixed errors in Dockerfile

--- idealhome/src/main/java/com/api/idealhome/IdealhomeApplication.java
+++ idealhome/src/main/java/com/api/idealhome/IdealhomeApplication.java
@@ -2,14 +2,13 @@ package com.api.idealhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
-public class IdealhomeApplication extends SpringBootServletInitializer {
+public class IdealhomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IdealhomeApplication.class, args);
